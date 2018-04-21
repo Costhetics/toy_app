a83877c37b57cdc74e7447c7bb3cd8650a6baafd
Adam Downer <adam@costhetics.com.au>
2018-04-21 15:27:00 +1000

    Added controller hello-world functionality & updated  readme.md

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    rand_num = Random.new.rand(0..2)
+    puts rand_num
+    if(rand_num < 1)
+      render html: "¡Hola, Mundo!" + rand_num.to_s
+    else
+      render :json => {"Test"=>"Test data", "Random number"=>rand_num.to_s}
+    end
+  end
+
+  def goodbye
+    render html:"Goodbye, world! :("
+  end
+
 end
